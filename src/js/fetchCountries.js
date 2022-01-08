ac222f5fefd25ea606bb54c7b16f0e9c343f57cb
Michael Paukov <michael.paukov@gmail.com>
2022-01-08 17:09:26 +0100

    task1

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,11 @@
 export default function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(response => {
-      return response.json();
-    })
-    .catch(error => {
-      return error;
-    });
+  const address = 'https://restcountries.com/v3.1/name/';
+  const options = `?fields=name,capital,population,flags,languages`;
+
+  return fetch(`${address}${name}${options}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
